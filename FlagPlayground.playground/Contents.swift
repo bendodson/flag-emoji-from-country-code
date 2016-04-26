9472bad8d1895d71cae8d3567154269131ab36b4
Ben Dodson <ben@bendodson.com>
2016-04-26 13:37:53 +0100

    Added playground

---
+++ FlagPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+
+import UIKit
+
+func emojiFlag(countryCode countryCode: String) -> Int {
+    var string = ""
+    var country = countryCode.uppercaseString
+    for uS in country.unicodeScalars {
+        string.append(UnicodeScalar(127397 + uS.value))
+    }
+    return string.characters.count
+}
+
+
+emojiFlag(countryCode: "gb")
+emojiFlag(countryCode: "im")
+emojiFlag(countryCode: "us")
+emojiFlag(countryCode: "es")
+emojiFlag(countryCode: "it")
+emojiFlag(countryCode: "se")
+emojiFlag(countryCode: "nz")
+emojiFlag(countryCode: "de")
+emojiFlag(countryCode: "eu")
+emojiFlag(countryCode: "en")
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# flag-emoji-from-country-code
-Swift function to return a flag emoji from an iSO 3166-1 country code
+# Flag Emoji from Country Code
+A simple Swift function to return a flag emoji from an iSO 3166-1 country code. For more details, see my blog post at https://bendodson.com/weblog/2016/04/26/emoji-flags-from-iso-3166-country-codes-in-swift/
